93f15251805fb3de4db2255432d4c3dc45cdbfa3
playerthomasm6 <69253499+playerthomasm6@users.noreply.github.com>
2020-10-04 15:45:04 -0400

    fixed Array problem JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,67 +1,76 @@
 // Assignment Code
 
-var Chrpool = {
-arrayOne: [],
-arrayTwo: [],
-arrayThree: [],
-arrayFour: []
-};
-
-
-
-
-
-
-
-
-
-
-
-
+var chrPool = [];
 
 //prompts start
 
-var selectlength = prompt("Select a password length between 10 and 128");
+document.getElementById("generate").onclick = generatePassword
 
-var abcloweryn = confirm("Do you want lower case letters?");
-  if (abcloweryn) {
-    var abclower = ["a", "b","c", "d", "e", "f", "g", "h", "i", "j", 
-                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
-                "u", "v", "w", "x", "y", "z"];
-    Chrpools.arrayOne.push(abclower);
-    console.log(chrpool);
-    }
+function generatePassword(){
 
-var abcupperyn = confirm("Do you want upper case letters?");
+var selectLength = prompt("Select a password length between 10 and 128");
+console.log(selectLength);
+var selectLengthInt = parseInt(selectLength);
+console.log(selectLength);
 
-  if (abcupperyn){
-    var abcupper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
-                "U", "V", "W", "X", "Y", "Z"];
-    Chrpool.push(abcupper);
-    console.log(chrpool);
-    }
+var abcLoweryn = confirm("Do you want lower case letters?");
+if (abcLoweryn) {
+  var abcLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+    "u", "v", "w", "x", "y", "z"];
+
+  chrPool.push(abcLower);
+  console.log(chrPool);
+  console.log(chrPool.length);
+}
+
+var abcUpperyn = confirm("Do you want upper case letters?");
+
+if (abcUpperyn) {
+  var abcUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+    "U", "V", "W", "X", "Y", "Z"];
+  chrPool.push(abcUpper);
+  console.log(chrPool);
+  console.log(chrPool.length);
+}
 
 var numyn = confirm("Do you want numbers?");
 
 if (numyn) {
   var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  Chrpool.push(num);
-  console.log(chrpool);
-  }
+  chrPool.push(num);
+  console.log(chrPool);
+  console.log(chrPool.length);
+}
 
-var spchryn = confirm("Do you want special characters?");
+var spChryn = confirm("Do you want special characters?");
 
-if (spchr) {
-  var spchr = ["!", "#", "$", "%", "&", "@"];
-  Chrpool.push(spchr);
-  console.log(chrpool);
-  }
+if (spChryn) {
+  var spChr = ["!", "#", "$", "%", "&", "@"];
+  chrPool.push(spChr);
+  console.log(chrPool);
+  console.log(chrPool.length);
+}
 
-  for (i=0; i < (selectlength + 1); i++){
-  var strselect = (Math.floor(Math.random() *(Chrpool.length)));
-  console.log(strselect);
 
-  console.log(Chrpool[strselect])
+for (i = 0; i < (selectLengthInt); i++) {
+  var strSelect = (Math.floor(Math.random() * (chrPool.length) ));
+  
+  //0, 1, 2, 3
+  var selection = chrPool[strSelect];
+  console.log(selection);
+  console.log(selection.length);
+  var selectionLength = selection.length;
+  
+  
+  strSelection = (Math.floor(Math.random() *selectionLength));
+  console.log(chrPool[strSelect][strSelection]);
+  var password = chrPool[strSelect][strSelection];  
 
-  }
+  var parElement = document.getElementById("password");
+  var textToAdd = document.createTextNode(password);
+  parElement.appendChild(textToAdd);
+
+}
+}
